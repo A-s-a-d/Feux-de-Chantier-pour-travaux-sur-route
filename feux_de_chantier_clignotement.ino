e17dcb6b91151d17f55aa682dace378e7b14dec3
A-s-a-d <81926998+A-s-a-d@users.noreply.github.com>
2021-10-04 12:03:14 +0200

    Add files via upload

---
+++ feux_de_chantier_clignotement.ino
@@ -0,0 +1,51 @@
+
+
+#define ledRg1 2
+#define ledVrt1 3
+#define ledRg2 4
+#define ledvrt2 8
+
+const word blinkDelay = 250;
+const word betweenBlinksDelay = 2000;
+
+void setup() {
+  pinMode(ledRg1, OUTPUT);
+  pinMode(ledVrt1, OUTPUT);
+  pinMode(ledRg2, OUTPUT);
+  pinMode(ledvrt2, OUTPUT);
+  digitalWrite(ledRg1, HIGH);
+  digitalWrite(ledVrt1, LOW);
+  digitalWrite(ledRg2, HIGH);
+  digitalWrite(ledvrt2, LOW);
+  delay(2000);
+  Serial.begin(115200);
+}
+
+void loop() {
+  delay(betweenBlinksDelay);
+  cycleRight();
+  delay(betweenBlinksDelay);
+  cycleLeft();
+}
+
+void cycleRight() {
+  digitalWrite(ledRg1, LOW);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(ledVrt1, HIGH);
+    delay(blinkDelay);                      
+    digitalWrite(ledVrt1, LOW);
+    delay(blinkDelay);                      
+  }
+  digitalWrite(ledRg1, HIGH);
+}
+
+void cycleLeft() {
+  digitalWrite(ledRg2, LOW);
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(ledvrt2, HIGH);
+    delay(blinkDelay);                      
+    digitalWrite(ledvrt2, LOW);
+    delay(blinkDelay);                       
+  }
+  digitalWrite(ledRg2, HIGH);
+}
